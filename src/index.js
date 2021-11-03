b39e76596299b46ea0e4b0830459d7e728afecea
Mo Chen <mochen0505@hotmail.com>
2021-11-04 01:42:17 +0800

    feat: sentry added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Launcher from '@morax/shield'
 import guid from '@morax/shield-guid'
+import sentry from '@morax/shield-sentry'
 import routes from './routes.config'
 
 if (module && module.hot) {
@@ -11,6 +12,7 @@ const app = new Launcher({
 })
 
 app.use(guid)
+app.use(sentry)
 
 app.start()
 
